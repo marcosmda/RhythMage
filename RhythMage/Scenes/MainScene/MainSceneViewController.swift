9c213d613fd99d7c85f82defd279a0a9d532bc77
Marcos Vinicius Majeveski De Angeli <marcosviniciusmda@gmail.com>
2021-10-04 13:06:18 -0300

    Organization of folders

---
+++ RhythMage/Scenes/MainScene/MainSceneViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainSceneViewController.swift
+//  RhythMage
+//
+//  Created by Marcos Vinicius Majeveski De Angeli on 01/10/21.
+//
+
+import UIKit
+
+class MainSceneViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        self.view.backgroundColor = .blue
+    }
+}
