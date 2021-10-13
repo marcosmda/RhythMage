353b2e19cd26b15ad3215d34955c92380900e660
julianaprado <julianaprado@me.com>
2021-10-13 15:05:37 -0300

    Loading Screen files created

---
+++ RhythMage/Scenes/LoadingScreen/Controller/LoadingScreenViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoadingScreenViewController.swift
+//  RhythMage
+//
+//  Created by Juliana Prado on 13/10/21.
+//
+
+import Foundation
---
+++ RhythMage/Scenes/LoadingScreen/View/LoadingScreenView.swift
@@ -0,0 +1,8 @@
+//
+//  LoadingScreenView.swift
+//  RhythMage
+//
+//  Created by Juliana Prado on 13/10/21.
+//
+
+import Foundation
