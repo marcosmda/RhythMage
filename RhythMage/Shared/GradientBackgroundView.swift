598fb1f16cab8c62a6553b7a3a736f6f92bc3955
Lucas Frazao <lucasnfrazao@icloud.com>
2021-10-18 19:07:24 -0300

    Add Basic Gradient Animation
    To be continued...

--- RhythMage/Shared/GradientBackgroundView.swift
+++ RhythMage/Shared/GradientBackgroundView.swift
@@ -9,8 +9,21 @@ import UIKit
 
 class GradientBackgroundView: UIView {
 
+    var timer: Timer?
+    
+    private var currentGradient: Int = 0
+    
+    let gradientLayer = CAGradientLayer()
+    var gradientSet = [[CGColor]]()
+    
+    let gradientOne = UIColor.primary.cgColor
+    let gradientTwo = UIColor.label.cgColor
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        gradientSet.append([gradientOne, gradientTwo])
+        gradientSet.append([gradientTwo, gradientOne])
+     
     }
     
     required init?(coder: NSCoder) {
@@ -18,13 +31,50 @@ class GradientBackgroundView: UIView {
     }
 
     func setupGradient(with view: UIView) {
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = [UIColor.primary.cgColor, UIColor.label.cgColor]
-        gradientLayer.locations = [0.0, 1.0]
-        gradientLayer.frame = view.bounds
+
+            gradientLayer.colors = gradientSet[0]
+        gradientLayer.locations = [0.35, 0.85]
+            gradientLayer.frame = view.bounds
+            gradientLayer.startPoint = CGPoint(x:1, y:0)
+            gradientLayer.endPoint = CGPoint(x:0, y:1)
+            gradientLayer.drawsAsynchronously = true
         
-        self.layer.insertSublayer(gradientLayer, at:0)
+            self.layer.insertSublayer(gradientLayer, at:0)
+        
+            animationBackground()
     }
     
+    @objc func animationBackground() {
+
+        if currentGradient < gradientSet.count - 1 {
+            currentGradient += 1
+        } else {
+            currentGradient = 0
+        }
+        
+        DispatchQueue.main.async {
+            let gradientChangeAnimation = CABasicAnimation(keyPath: "colors")
+            gradientChangeAnimation.delegate = self
+            gradientChangeAnimation.duration = 7.0
+            gradientChangeAnimation.toValue = self.gradientSet[self.currentGradient]
+            gradientChangeAnimation.fillMode = CAMediaTimingFillMode.forwards
+            gradientChangeAnimation.isRemovedOnCompletion = false
+            self.gradientLayer.add(gradientChangeAnimation, forKey: "colorChange")
+        }
+        
+        
+    }
+    
+    
+}
+
+extension GradientBackgroundView: CAAnimationDelegate {
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+            if flag {
+                gradientLayer.colors = gradientSet[currentGradient]
+                animationBackground()
+            }
+        }
     
 }
