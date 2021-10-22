f4876b444dec7fafe0fc3caac6d07f989b01bd4c
Bruna Costa <brunafranca704@gmail.com>
2021-10-22 15:08:45 -0300

    Readded the Settings Scene and the Credits Scene to the project. Added the User Settings file so we can save the status of the haptic button. Fixed the navigation of the settings button to the SmileToUnlockView. Created the App Container for the views.

---
+++ RhythMage/Models/UseSettings.swift
@@ -0,0 +1,14 @@
+//
+//  UseSettings.swift
+//  RhythMage
+//
+//  Created by Bruna Costa on 22/10/21.
+//
+
+import Foundation
+
+class UserSettings{
+    
+    var isHapticOn: Bool = true
+    var user = User(id: "1", name: "Carlos")
+}
---
+++ RhythMage/Scenes/CreditsScene/Controller/CreditsSceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  CreditsSceneDelegate.swift
+//  RhythMage
+//
+//  Created by Bruna Costa on 22/10/21.
+//
+
+import Foundation
+import UIKit
+
+protocol CreditsSceneDelegate {
+    func onBackButtonPush()
+}
