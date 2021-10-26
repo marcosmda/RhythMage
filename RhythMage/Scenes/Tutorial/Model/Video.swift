22a2f920ad4a8cce855461401037a4e957f9d525
Lucas Frazao <lucasnfrazao@icloud.com>
2021-10-26 16:32:38 -0300

    Updated video and timestamps

--- RhythMage/Scenes/Tutorial/Model/Video.swift
+++ RhythMage/Scenes/Tutorial/Model/Video.swift
@@ -13,12 +13,12 @@ struct Video {
     var subtitles: [Double:String]
     
     static func setOnboarding() -> Video {
-        return Video(title: "rhythMage", subtitles: [
+        return Video(title: "rhtyhMage", subtitles: [
             0.0 : "At the top of the icy mountains, there is a very clumsy mage, who always works his magic to different music.",
-            9.0 : "He liked the songs so much that he got distracted and never wrote down his new spells.",
-            15.0 : "One day, he went to do a new spell that went terribly wrong, and all the spells he remembered came out of his head, floating in different rhythms!",
-            26.0: "Thus, his spells were adrift and he was never able to retrieve them alone. He thought it would be impossible to remember them.",
-            34.0: "Until you... oh, wait. Where are you?"
+            8.30 : "He liked the songs so much that he got distracted and never wrote down his new spells.",
+            13.0 : "One day, he went to do a new spell that went terribly wrong, and all the spells he remembered came out of his head, floating in different rhythms!",
+            24.0: "Thus, his spells were adrift and he was never able to retrieve them alone. He thought it would be impossible to remember them.",
+            32.0: "Until you... oh, wait. Where are you?"
         ])
     }
     
