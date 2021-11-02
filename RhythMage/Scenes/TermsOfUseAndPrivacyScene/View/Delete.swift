ac6e8e2929341bc559d1e62bd73e5aaf1278f8dd
Bruna Costa <brunafranca704@gmail.com>
2021-11-02 12:50:15 -0300

    push to main for frazao to mix

---
+++ RhythMage/Scenes/TermsOfUseAndPrivacyScene/View/Delete.swift
@@ -0,0 +1,15 @@
+//
+//  Delete.swift
+//  RhythMage
+//
+//  Created by Bruna Costa on 02/11/21.
+//
+
+import Foundation
+import UIKit
+
+class DeleteFile: UIView {
+    
+    var terms =  TermsOfUseDelegate.self
+}
+
