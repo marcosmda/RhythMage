866c750e125a3d459f6f6b6d719d3533492facb4
Bruna Costa <brunafranca704@gmail.com>
2021-11-11 21:47:37 -0300

    remade the credits view

--- RhythMage/Scenes/CreditsScene/View/CreditsView.swift
+++ RhythMage/Scenes/CreditsScene/View/CreditsView.swift
@@ -13,8 +13,10 @@ class CreditsView: UIView{
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.addSubview(gradientView)
-        self.addSubview(creditsText)
         setUpView()
+        setUpScrollView()
+        //scrollViewDidScroll(scrollView)
+        
     }
     
     required init?(coder: NSCoder) {
@@ -37,7 +39,6 @@ class CreditsView: UIView{
         return barButtonItem
     }()
     
-    
     ///Create a title of the section of the terms of use to appear in the nav bar
     let titleNavBar: DynamicLabel = {
         let label = DynamicLabel()
@@ -50,26 +51,33 @@ class CreditsView: UIView{
         label.text = "CREDITS".uppercased()
         return label
     }()
+
     
-    let creditsText: DynamicLabel = {
-        let label1 = DynamicLabel()
-        label1.translatesAutoresizingMaskIntoConstraints = false
-        label1.textColor = .secondary
-        label1.text = "THIS GAME WAS MADE IN BRAZIL BY (NOT SO) HAPPY DEVELOPERS"
-        label1.font = .inikaBold(ofSize: 25)
-        label1.numberOfLines = 0
-        label1.textAlignment = .center
-        label1.contentMode = .scaleAspectFit
-        label1.adjustsFontSizeToFitWidth = true
-        return label1
+    var scrollView = UIScrollView()
+    var contentView = UIView()
+    
+    
+    ///Create the table view of the buttons bellow the haptic button
+    let tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(RhythMageLogo.self, forCellReuseIdentifier: RhythMageLogo.reusableIdentifier)
+        table.register(NoCashLogo.self, forCellReuseIdentifier: NoCashLogo.reusableIdentifier)
+        table.register(AutorsCredits.self, forCellReuseIdentifier: AutorsCredits.reusableIdentifier)
+        table.backgroundColor = .clear
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.separatorStyle = .none
+        table.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        table.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+        table.rowHeight = UITableView.automaticDimension
+        table.estimatedRowHeight = 54.00
+        //table.isScrollEnabled = false
+        //table.layer.cornerRadius = 20
+        return table
     }()
     
+    
+    
     func setUpView(){
-        creditsText.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        creditsText.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        creditsText.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -60).isActive = true
-        creditsText.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 60).isActive = true
-        
         gradientView.translatesAutoresizingMaskIntoConstraints = false
         gradientView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         gradientView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
@@ -77,10 +85,66 @@ class CreditsView: UIView{
         gradientView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         
         gradientView.setupCircleBackgroundBlur()
-
+    }
+    
+    func setUpScrollView(){
+        
+       // self.addSubview(scrollView)
+        self.addSubview(tableView)
+        //scrollView.addSubview(contentView)
+        //contentView.addSubview(tableView)
+        
+        //scrollView.bounces = false
+       // tableView.bounces = false
+        
+        /*
+        scrollView.backgroundColor = .red
+        contentView.backgroundColor = .blue
+        tableView.backgroundColor = .green
+        */
+        //scrollView.addSubview(tableView)
+        
+        //scrollView.translatesAutoresizingMaskIntoConstraints = false
+        //contentView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        /*
+        scrollView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+       
+        contentView.topAnchor.constraint(equalTo: scrollView.layoutMarginsGuide.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: scrollView.layoutMarginsGuide.trailingAnchor).isActive = true
+        contentView.leadingAnchor.constraint(equalTo: scrollView.layoutMarginsGuide.leadingAnchor).isActive = true
+        contentView.heightAnchor.constraint(equalTo: scrollView.heightAnchor).isActive = true
+       
+        
+        tableView.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor).isActive = true */
+        //tableView.heightAnchor.constraint(equalTo: contentView.heightAnchor).isActive = true
+        
+        tableView.topAnchor.constraint(equalTo: self.layoutMarginsGuide.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        //tableView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
     }
     
     @objc func onBackButtonPush(){
         delegate?.onBackButtonPush()
     }
+  /*
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView == self.scrollView {
+            tableView.isScrollEnabled = (self.scrollView.contentOffset.y >= 200)
+        }
+
+        if scrollView == self.tableView {
+            self.tableView.isScrollEnabled = (tableView.contentOffset.y > 0)
+        }
+    }*/
 }
